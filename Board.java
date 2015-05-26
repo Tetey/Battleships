8621ff5872949a6d0fdb9b6c01b0374bc8d72237
Tetey <aglagunda@up.edu.ph>
2015-05-26 21:56:35 +0800

    Something wrong with my previous commit

--- Board.java
+++ Board.java
@@ -7,17 +7,25 @@ import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
+import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
 
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JPanel;
 import javax.swing.KeyStroke;
 
 
 public class Board extends JFrame implements ActionListener{
 	private static final long serialVersionUID = -7193663594390472688L;
 	private static final int TILELENGTH = 35;
+	private static final int STARTXBORDER1 = 30;
+	private static final int STARTXBORDER2 = STARTXBORDER1+(11*TILELENGTH);
+	private static final int STARTYBORDER = 33-24;
 	private String status = "init";
 	public Board(){
 		super("Battleships");
@@ -27,7 +35,7 @@ public class Board extends JFrame implements ActionListener{
 		JMenuItem instructions = new JMenuItem("Instructions");
 		JMenuItem exit = new JMenuItem("Exit");
 		JMenuBar bar = new JMenuBar();
-
+		GamePanel panel = new GamePanel();
 		file.setMnemonic(KeyEvent.VK_F);
 		newgame.setMnemonic(KeyEvent.VK_N);
 
@@ -35,7 +43,7 @@ public class Board extends JFrame implements ActionListener{
 		instructions.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_I, ActionEvent.CTRL_MASK));
 		exit.addActionListener(this);
 		exit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F4, ActionEvent.ALT_MASK));
-		file.addActionListener(this);
+		//file.addActionListener(new JMenuActionListener(this));
 		
 		file.add(newgame);
 		file.add(instructions);
@@ -48,40 +56,18 @@ public class Board extends JFrame implements ActionListener{
         
 		Container con = this.getContentPane();
 		con.setLayout(new FlowLayout());
+		setContentPane(panel);
 		setSize(800, 400);
+		
 	        
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setResizable(false);
         setVisible(true);    
 		
-		MouseReader mouse = new MouseReader();
-		addMouseListener(mouse);
-		addMouseMotionListener(mouse);
-				
+		
 	}
 	
 	
-	
-	//Draw functions
-	public void paint(Graphics g){
-		super.paint(g);
-		if(!status.equals("end")){
-			drawBackgroundImage(g);
-			drawGridLines(g);
-			//drawBoats();
-		}
-		else{
-			//drawResult();
-		}
-	}
-	
-	private void drawBackgroundImage(Graphics g){
-		//g.drawImage("images"+File.separator +"bgimage.",0,0,643,624,null);
-	}
-	private void drawGridLines(Graphics g){
-		g.drawRect(30, 33, 10*TILELENGTH, 10*TILELENGTH);
-		g.drawRect(30+(11*TILELENGTH), 33, 10*TILELENGTH, 10*TILELENGTH);
-	}
 	/*
 	protected void drawExtra (Graphics g)
 	{
@@ -294,15 +280,65 @@ public class Board extends JFrame implements ActionListener{
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
 		
 	}
 
-	private class MouseReader extends MouseAdapter implements MouseMotionListener{
+
+	private class GamePanel extends JPanel implements Serializable {
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 2080838606441713101L;
+		public GamePanel(){
+			MouseReader mouse = new MouseReader();
+			addMouseListener(mouse);
+			addMouseMotionListener(mouse); 
+		}
+		
+		//Draw functions
+		public void paint(Graphics g){
+			super.paintComponents(g);
+			if(!status.equals("end")){
+				drawBackgroundImage(g);
+				drawGridLines(g);
+				//drawBoats();
+			}
+			else{
+				//drawResult();
+			}
+		}
+		
+		private void drawBackgroundImage(Graphics g){
+			String filename = "";
+			try {
+				filename = "Images"+File.separator +"bgimage.png";
+				g.drawImage(ImageIO.read( new File(filename)),0,0,800,400,null);
+			} catch (IOException e) {
+				System.out.println(filename);
+			}
+		}
+		private void drawGridLines(Graphics g){
 			
-		public void mouseClicked(MouseEvent e){
-			System.out.println("x: " + e.getX() + " y: " + e.getY());
-			repaint();
+			g.fillRect(STARTXBORDER1-1, STARTYBORDER-1, 10*TILELENGTH+1, 10*TILELENGTH+1);
+			g.fillRect(STARTXBORDER2-1, STARTYBORDER-1, 10*TILELENGTH+1, 10*TILELENGTH+1);
+			g.clearRect(STARTXBORDER1, STARTYBORDER, 10*TILELENGTH-1, 10*TILELENGTH-1);
+			g.clearRect(STARTXBORDER2, STARTYBORDER, 10*TILELENGTH-1, 10*TILELENGTH-1);
+			g.drawRect(STARTXBORDER1, STARTYBORDER, 10*TILELENGTH, 10*TILELENGTH);
+			g.drawRect(STARTXBORDER2, STARTYBORDER, 10*TILELENGTH, 10*TILELENGTH);
+			
+			for(int i = 1; i <= 9; i++){
+				g.drawLine(STARTXBORDER1+(i*TILELENGTH),STARTYBORDER,STARTXBORDER1+(i*TILELENGTH),STARTYBORDER+10*TILELENGTH);
+				g.drawLine(STARTXBORDER2+(i*TILELENGTH),STARTYBORDER,STARTXBORDER2+(i*TILELENGTH),STARTYBORDER+10*TILELENGTH);
+				g.drawLine(STARTXBORDER1,STARTYBORDER+i*TILELENGTH,STARTXBORDER1+10*TILELENGTH,STARTYBORDER+i*TILELENGTH);
+				g.drawLine(STARTXBORDER2,STARTYBORDER+i*TILELENGTH,STARTXBORDER2+10*TILELENGTH,STARTYBORDER+i*TILELENGTH);
+			}
+		}
+		private class MouseReader extends MouseAdapter implements MouseMotionListener{
+			public void mouseClicked(MouseEvent e){
+				repaint();
+				System.out.println("x: " + e.getX() + " y: " + e.getY());
+			}
 		}
 		
 	}
